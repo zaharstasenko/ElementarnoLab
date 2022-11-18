5eb3b5fa3e7daadc78975c63da67d0989bc261b1
Zakharii <zax.stas@gmail.com>
2022-11-18 19:35:59 +0200

    [ELA-2] Configure Firebase Analytics

--- app/src/main/java/com/elementarno/lab/MainActivity.kt
+++ app/src/main/java/com/elementarno/lab/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.elementarno.lab
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.widget.ImageView
-import android.widget.Toast
-import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
